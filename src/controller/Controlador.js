16ee9fa5dc967d5e8473bd78ae652861f008dff9
Kleiton Lima <kleiton.mini@gmail.com>
2022-05-04 21:13:38 -0300

    Criada conexões com o database

--- src/controller/Controlador.js
+++ src/controller/Controlador.js
@@ -1,3 +1,11 @@
-export const getIndex = (req, res) => {
+import { connection } from "../database/connection.js";
+import { personagens } from "../model/personagens.js";
+
+export const getIndex = async (req, res) => {
+  // const listPersonagens = await connection.query("SELECT * FROM personagens", {
+  //   model: personagens,
+  // });
+  const listPersonagens = await personagens.findAll();
+  console.log(listPersonagens);
   res.render("index.ejs");
 };
---
+++ src/model/personagens.js
@@ -0,0 +1,40 @@
+import Sequelize from "sequelize";
+import { connection } from "../database/connection.js";
+
+export const personagens = connection.define(
+  "personagens",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    imagem: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    nome: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    especie: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    habilidades: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    personalidade: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: true,
+    createdAt: false,
+    updatedAt: false,
+    timestamps: false,
+  }
+);
