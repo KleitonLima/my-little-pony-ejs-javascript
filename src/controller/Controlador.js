74beb2a4196037448bab320d24abeaf783f5a4c5
Kleiton Lima <kleiton.mini@gmail.com>
2022-05-04 23:11:04 -0300

    Insert try catch

--- src/controller/Controlador.js
+++ src/controller/Controlador.js
@@ -2,10 +2,14 @@ import { connection } from "../database/connection.js";
 import { personagens } from "../model/personagens.js";
 
 export const getIndex = async (req, res) => {
-  // const listPersonagens = await connection.query("SELECT * FROM personagens", {
-  //   model: personagens,
-  // });
-  const listPersonagens = await personagens.findAll();
-  console.log(listPersonagens);
-  res.render("index.ejs");
+  try {
+    // const listPersonagens = await connection.query("SELECT * FROM personagens", {
+    //   model: personagens,
+    // });
+    const listPersonagens = await personagens.findAll();
+    console.log(listPersonagens);
+    res.render("index.ejs");
+  } catch (error) {
+    res.send(error.message);
+  }
 };
