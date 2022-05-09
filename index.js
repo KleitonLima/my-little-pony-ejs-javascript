1cc32c7c8043864108b4f2ac70a060c268354c98
Kleiton Lima <kleiton.mini@gmail.com>
2022-05-09 20:55:44 -0300

    inclusão do dotenv

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import { routers } from "./src/routers/routers.js";
 import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const port = 3223;
+const port = 3223 || process.env.PORT;
 const __dirname = path.resolve(path.dirname(""));
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 app.set("view engine", "ejs");
 app.use(routers);
 app.use(express.static(path.join(__dirname, "public")));
