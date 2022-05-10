e9876f5f56210c65c4410c95ea55646c5ff76744
Kleiton Lima <kleiton.mini@gmail.com>
2022-05-09 23:06:03 -0300

    database update

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const connection = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-});
+export const connection = new Sequelize(
+  "postgres://personagens_my_little_pony_user:95dPlCdFokTznjLSi382iXWFUK0czG22@dpg-c9sq8tnh8vl0smtrbcqg-a.oregon-postgres.render.com/personagens_my_little_pony",
+  {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
